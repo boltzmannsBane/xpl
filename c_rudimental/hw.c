ba9f2086a5146ac9c77e16d05c6738d945ecacc8
Levan Apkhazava <levan.apkhazava@scandiweb.com>
2023-06-27 04:58:51 +0400

    rip bozo

--- c_rudimental/hw.c
+++ c_rudimental/hw.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+  int x;
+  int y;
+} point;
 
 int main() {
-    printf("Hello World!\n");
-    return 0;
+  point * mypoint = NULL;
+
+  mypoint = (point *) malloc(sizeof(point))
+
+  /* Dynamically allocate a new point
+     struct which mypoint points to here */
+
+  mypoint->x = 10;
+  mypoint->y =5 ;
+  printf("mypoint coordinates: %d, %d\n", mypoint->x, mypoint->y);
+
+  free(mypoint);
+  return 0;
 }
