650e9bc0726f94d4dc4458235266dcce6e35ed57
amygdala <leonapkhazava@gmail.com>
2023-08-14 17:48:13 +0400

    rip bozo

--- cbook/sizes.c
+++ cbook/sizes.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-#include <limits.h>
 
 int main() {
-  printf("%u\n", INT_MAX);
+  printf("Size of int: %zu bytes\n", sizeof(int));
+
+  unsigned int max_unsigned_int = ~0;
+  int max_int = (int)(max_unsigned_int >> 1);
+  int min_int = ~max_int;
+
+  printf("Range of int: %d to %d\n", max_int, min_int);
+
+  return 0;
 }
